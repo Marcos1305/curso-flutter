19039b9035fd2e3e1ecdf63741a94bbebb844ff3
Marcos1305 <marcos1305pt@gmail.com>
2020-02-06 21:23:35 -0300

    Terminado app de gifs

--- buscador_gifs/lib/main.dart
+++ buscador_gifs/lib/main.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:buscar_gifs/ui/home_page.dart';
+
 const API_KEY = 'q6CKMdjApTKWY35Eo63GZGfC3mbWqHh8';
+
+void main() {
+  runApp(MaterialApp(
+      home: HomePage(),
+      theme: ThemeData(
+          hintColor: Colors.white,
+          primaryColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white))))));
+}
