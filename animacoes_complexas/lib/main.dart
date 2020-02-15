755c6c4e997efdc9d3f394ffdec6f8ffa9574bfc
Marcos1305 <marcos1305pt@gmail.com>
2020-02-15 16:59:09 -0300

    Basico de animacoes

--- animacoes_complexas/lib/main.dart
+++ animacoes_complexas/lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Animações',
+      home: LogoApp(),
     );
   }
 }
@@ -19,6 +20,7 @@ class LogoApp extends StatefulWidget {
 class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   AnimationController controller;
   Animation<double> animation;
+  Animation<double> animation2;
 
   @override
   void initState() {
@@ -26,7 +28,13 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     controller =
         AnimationController(vsync: this, duration: Duration(seconds: 2));
 
-    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation = CurvedAnimation(parent: controller, curve: Curves.elasticOut);
+
+    animation.addListener(() {
+      print(animation.value);
+    });
+
+    controller.forward();
   }
 
   @override
@@ -37,6 +45,61 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: CustomTransition(child: LogoWidget(), animation: animation));
+  }
+}
+
+// class AnimatedLogo extends AnimatedWidget {
+//   AnimatedLogo(Animation<double> animation) : super(listenable: animation);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final Animation<double> animation = listenable;
+
+//     return Center(
+//       child: Container(
+//         height: animation.value,
+//         width: animation.value,
+//         child: FlutterLogo(),
+//       ),
+//     );
+//   }
+// }
+
+class LogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FlutterLogo(),
+    );
+  }
+}
+
+class CustomTransition extends StatelessWidget {
+  final Widget child;
+  final Animation<double> animation;
+
+  final sizeTween = Tween<double>(begin: 0, end: 300);
+  final opacityTween = Tween<double>(begin: 0.1, end: 1);
+
+  CustomTransition({this.child, this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return Opacity(
+          opacity: opacityTween.evaluate(animation).clamp(0.0, 1.0),
+          child: Container(
+            height: sizeTween.evaluate(animation),
+            width: sizeTween.evaluate(animation),
+            child: child,
+          ),
+        );
+      },
+      child: child,
+    );
   }
 }
